eacda114c3a9376a0b28548f62529b4941b9f9c9
anik121 <rifatislam30@gmail.com>
2020-08-07 00:35:00 +0600

    home page complete without some feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
 (function($){
+    feather.replace();
+    // set a deafult value in a tag
+    $("a[href='#']").attr('href','javascript:void(0)')
     $('.foodLog-slider').owlCarousel({
         items:1,
         loop: true,
         margin: 10
-    })
+    });
+
+    $('.foodLog-tabItem > li').on('click',function(){
+        $('.foodLog-tabItem > li').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.foodLog-navbar ul > li').on('click',function(){
+        $('.foodLog-navbar ul > li').removeClass('active');
+        $(this).addClass('active');
+    });
+    
 })(jQuery);
