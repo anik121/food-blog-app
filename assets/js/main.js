aaef0bdc24e3eaa8ac28439bac27b66f2f827d79
anik121 <rifatislam30@gmail.com>
2020-08-11 16:05:32 +0600

    add sinigle page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,39 @@
-(function($){
-    feather.replace();
-    // set a default value in a tag
-    $("a[href='#']").attr('href','javascript:void(0)')
+(function ($) {
+  feather.replace();
+  // set a default value in a tag
+  $("a[href='#']").attr("href", "javascript:void(0)");
 
-    // For Sidebar Opening And CLosing
-    $('.foodLog-menu').on('click',function(){
-        $('.sidebar').removeClass('none-showing');
-    });
-    $('.close-sidebar').on('click',function(){
-        $('.sidebar').addClass('none-showing');
-    });
+  // For Sidebar Opening And CLosing
+  $(".foodLog-menu").on("click", function () {
+    $(".sidebar").removeClass("none-showing");
+  });
+  $(".close-sidebar").on("click", function () {
+    $(".sidebar").addClass("none-showing");
+  });
 
-    $('.foodLog-slider').owlCarousel({
-        items:1,
-        loop: true,
-        margin: 10
-    });
+  $(".foodLog-post-rating").on("click", function () {
+    $(this).toggleClass("active");
+  });
 
-    $('.foodLog-tabItem > li').on('click',function(){
-        $('.foodLog-tabItem > li').removeClass('active');
-        $(this).addClass('active');
-    });
+  $(".foodLog-slider").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+  });
 
-    $('.foodLog-navbar ul > li').on('click',function(){
-        $('.foodLog-navbar ul > li').removeClass('active');
-        $(this).addClass('active');
-    });
+  $(".foodLog-tabItem > li").on("click", function () {
+    $(".foodLog-tabItem > li").removeClass("active");
+    $(this).addClass("active");
+  });
 
-    $('.menu-list > li').on('click',function(){
-        $('.menu-list > li').removeClass('active');
-        $(this).addClass('active');
-        $('.sidebar').addClass('none-showing');
-    });
-    
+  $(".foodLog-navbar ul > li").on("click", function () {
+    $(".foodLog-navbar ul > li").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $(".menu-list > li").on("click", function () {
+    $(".menu-list > li").removeClass("active");
+    $(this).addClass("active");
+    $(".sidebar").addClass("none-showing");
+  });
 })(jQuery);
